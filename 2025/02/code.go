9def1e92be513f5b39bcf9522ba697ab8dbfa592
Mike Benson <mike@kambe.com.au>
2025-12-06 23:21:53 +1100

    Completed 2025, Day 02, Part 2

--- 2025/02/code.go
+++ 2025/02/code.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -24,65 +25,94 @@ func run(part2 bool, input string) any {
 	idRanges := strings.Split(strings.TrimSpace(input), ",")
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return "not implemented"
+		for _, idRange := range idRanges {
+			bounds := strings.Split(idRange, "-")
+			lenFirst := len(bounds[0])
+			lenLast := len(bounds[1])
+			cache := []int{}
+			for repeat := 2; repeat <= lenLast; repeat++ {
+				var result int
+				// We need to return the cache, since the cache header is passed by value,
+				// and when the cache size changes, the header needs to change in this scope as well
+				result, cache = findInvalids(repeat, bounds[0], lenFirst, bounds[1], lenLast, cache)
+				// fmt.Println(" ", idRange, repeat, result)
+				total += result
+			}
+		}
+		return strconv.Itoa(total)
 	}
 	// solve part 1 here
 	for _, idRange := range idRanges {
 		bounds := strings.Split(idRange, "-")
-		// Narrow the search space if we can
 		lenFirst := len(bounds[0])
 		lenLast := len(bounds[1])
-		// The range is the same odd number of digits at each end, so we can skip it
-		repeat := 2
-		if (lenFirst == lenLast) && ((lenFirst % repeat) > 0) {
-			continue
-		}
-		first, _ := strconv.Atoi(bounds[0])
-		last, _ := strconv.Atoi(bounds[1])
-		// Narrow the lower bound
-		if (lenFirst % repeat) > 0 { // first contains an odd number of digits
-			j := int(math.Pow(10.0, float64(lenFirst)))
-			if j <= last {
-				first = j
-			} else {
-				continue
-			}
-		}
-		// Narrow the upper bound
-		if (lenLast % 2) > 0 { // last contains an odd number of digits
-			j := int(math.Pow(10.0, float64(lenLast-1)) - 1)
-			if j >= first {
-				last = j
-			} else {
-				continue
-			}
-		}
-		firstString := strconv.Itoa(first)
-		lastString := strconv.Itoa(last)
-		// this is the length of our substring
-		// (note the implicit assumption that firstString and lastString are the same length!)
-		lenSub := len(firstString) / repeat
-		// create an approximate range of substrings
-		min, _ := strconv.Atoi(firstString[:lenSub])
-		max, _ := strconv.Atoi(lastString[:lenSub])
-		// the scale factor converts our substring into the full string with repeated strings of digits
-		scale := int(math.Pow(10.0, float64(lenSub))) + 1
-		// Compute all the possible "invalid" product ids in our final range
-		for i := min; i <= max; i++ {
-			checkInt := i * scale
-			if checkInt >= first {
-				if checkInt <= last {
-					// Goldilocks!
-					total += checkInt
-				} else {
-					// we are out of the range, we can stop
-					break
-				}
-			} else {
-				// keep going until we are in the range (or we run out of numbers)
-				continue
-			}
-		}
+		cache := []int{}
+		var result int
+		result, _ = findInvalids(2, bounds[0], lenFirst, bounds[1], lenLast, cache)
+		total += result
 	}
 	return strconv.Itoa(total)
+}
+
+func findInvalids(repeat int, lowerBound string, lenFirst int, upperBound string, lenLast int, cache []int) (int, []int) {
+	total := 0
+	// Narrow the search space if we can
+	// The range is the same number of digits at each end, and can't be evenly divided by the pattern size we're using, so we can skip it
+	if (lenFirst == lenLast) && ((lenFirst % repeat) > 0) {
+		return 0, cache
+	}
+	first, _ := strconv.Atoi(lowerBound)
+	last, _ := strconv.Atoi(upperBound)
+	// Narrow the lower bound
+	if (lenFirst % repeat) > 0 { // number of digits in first can't be evenly divided by the pattern size
+		j := int(math.Pow(10.0, float64(lenFirst)))
+		if j <= last {
+			first = j
+		} else {
+			return 0, cache
+		}
+	}
+	// Narrow the upper bound
+	if (lenLast % repeat) > 0 { // last contains an odd number of digits
+		j := int(math.Pow(10.0, float64(lenLast-1)) - 1)
+		if j >= first {
+			last = j
+		} else {
+			return 0, cache
+		}
+	}
+	firstString := strconv.Itoa(first)
+	lastString := strconv.Itoa(last)
+	// this is the length of our substring
+	// (note the implicit assumption that firstString and lastString are the same length!)
+	lenSub := len(firstString) / repeat
+	// create an approximate range of substrings
+	min, _ := strconv.Atoi(firstString[:lenSub])
+	max, _ := strconv.Atoi(lastString[:lenSub])
+	// the scale factor converts our substring into the full string with repeated strings of digits
+	scale := 1
+	for f := 1; f < repeat; f++ {
+		scale += int(math.Pow(10.0, float64(lenSub*f)))
+	}
+	// scale := int(math.Pow(10.0, float64(lenSub))) + 1
+	// Compute all the possible "invalid" product ids in our final range
+	for i := min; i <= max; i++ {
+		checkInt := i * scale
+		if checkInt >= first {
+			if checkInt <= last {
+				// Goldilocks!
+				if !slices.Contains(cache, checkInt) {
+					total += checkInt
+					cache = append(cache, checkInt)
+				}
+			} else {
+				// we are out of the range, we can stop
+				break
+			}
+		} else {
+			// keep going until we are in the range (or we run out of numbers)
+			continue
+		}
+	}
+	return total, cache
 }
